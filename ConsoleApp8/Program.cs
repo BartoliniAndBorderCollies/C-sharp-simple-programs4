dad117d7fa24b504aca56db7503f9ba26a709095
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-11-19 14:38:23 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Program rysujący prostokąt z gwiazdek o zadanych przez usera wymiarach, dla a i b");
+
+Console.WriteLine("Podaj szerokosć");
+int szerokosc = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Podaj wysokosc");
+int wysokosc = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < wysokosc; i++)
+{
+    for(int j = 0; j < szerokosc; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
